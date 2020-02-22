a32dc63df658b16c43394809f4057f12fa5819ae
Glutanimate <glutanimate@users.noreply.github.com>
2020-02-22 13:24:12 +0100

    Update Python target to >=3.6

--- setup.py
+++ setup.py
@@ -23,20 +23,16 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "Operating System :: POSIX",
         "Operating System :: MacOS",
     ],
     keywords='anki development build-tools',
     packages=["aab"],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
-    install_requires=["six", "jsonschema", "whichcraft",
-                      "pathlib;python_version<'3.4'"],
+    python_requires='>=3.6, <4',
+    install_requires=["six", "jsonschema", "whichcraft"],
     # e.g. $ pip install aab[anki21]
     extras_require={
         'anki21': ['PyQt5>=5.12']
