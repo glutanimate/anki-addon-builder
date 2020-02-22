f9004b69751429030a9ed82d2d51f0446b06b085
Glutanimate <glutanimate@users.noreply.github.com>
2020-02-22 14:32:43 +0100

    Bump version: 0.1.4 → 1.0.0-dev.1

--- aab/__init__.py
+++ aab/__init__.py
@@ -50,7 +50,7 @@ from pathlib import Path
 
 # Meta
 
-__version__ = "0.1.4"
+__version__ = "1.0.0-dev.1"
 __author__ = "Aristotelis P. (Glutanimate)"
 __title__ = "Anki Add-on Builder"
 __homepage__ = "https://glutanimate.com"
