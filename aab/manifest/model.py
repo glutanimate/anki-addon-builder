715afc692c73f6114e69cd21d9511af86f6fb97f
Glutanimate <glutanimate@users.noreply.github.com>
2021-10-17 22:28:14 +0200

    Constrain ankiweb_id, format model

--- aab/manifest/model.py
+++ aab/manifest/model.py
@@ -9,27 +9,25 @@ class AddonManifest(BaseModel):
     package: str = Field(
         ...,
         description=(
-            "The module/package the add-on is imported as, i.e. the name of the add-on"
-            " folder."
+            "The package the add-on is imported as, i.e. the name of the add-on folder."
         ),
         min_length=1,
     )
     name: str = Field(
         ...,
-        description="The name displayed in Anki's UI (e.g. in the add-on list)",
+        description="The name displayed in Anki's GUI (e.g. in the add-on list)",
         min_length=1,
     )
     mod: Optional[int] = Field(
-        default=None, description="The time the add-on was last modified (unix epoch)"
+        default=None,
+        description="The time the add-on was last modified (unix timestamp in seconds)",
     )
     conflicts: Optional[List[str]] = Field(
         default=None,
-        description="A list of other packages that conflict",
-        title="Conflicting Add-ons",
+        description="A list of other packages that conflict with the add-on",
     )
     min_point_version: Optional[int] = Field(
-        default=None, description="the minimum 2.1.x version this add-on supports",
-        gt=0
+        default=None, description="the minimum 2.1.x version this add-on supports", gt=0
     )
     max_point_version: Optional[int] = Field(
         default=None,
@@ -50,14 +48,13 @@ class AddonManifest(BaseModel):
         default=None,
         description="Homepage of the add-on project (e.g. GitHub repository link)",
     )
-    
+
     @validator("conflicts", each_item=True)
     def check_conflicts(cls, conflict: str, values: dict):
         if len(conflict) < 1:
             raise ValueError("Conflict specifier cannot be an empty string.")
         if conflict == values.get("package"):
             raise ValueError("Add-on can not conflict with itself.")
-            
 
 
 class ExtendedAddonManifest(AddonManifest):
@@ -66,7 +63,7 @@ class ExtendedAddonManifest(AddonManifest):
         description="[aab] Version string, set to the same value as human_version",
     )
     ankiweb_id: Optional[str] = Field(
-        default=None, description="[aab] The AnkiWeb upload ID."
+        default=None, description="[aab] The AnkiWeb upload ID.", regex=r"[0-9]+"
     )
     author: Optional[str] = Field(
         default=None,
