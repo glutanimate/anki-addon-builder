9f643b7a46c993204594032c6f62abe5b5c239dc
Glutanimate <glutanimate@users.noreply.github.com>
2021-10-17 20:18:22 +0200

    Fortify manifest model

--- aab/manifest/model.py
+++ aab/manifest/model.py
@@ -12,11 +12,14 @@ class AddonManifest(BaseModel):
             "The module/package the add-on is imported as, i.e. the name of the add-on"
             " folder."
         ),
+        min_length=1,
     )
     name: str = Field(
-        ..., description="The name displayed in Anki's UI (e.g. in the add-on list)"
+        ...,
+        description="The name displayed in Anki's UI (e.g. in the add-on list)",
+        min_length=1,
     )
-    mod: Optional[float] = Field(
+    mod: Optional[int] = Field(
         default=None, description="The time the add-on was last modified (unix epoch)"
     )
     conflicts: Optional[List[str]] = Field(
@@ -24,17 +27,18 @@ class AddonManifest(BaseModel):
         description="A list of other packages that conflict",
         title="Conflicting Add-ons",
     )
-    min_point_version: Optional[float] = Field(
-        default=None, description="the minimum 2.1.x version this add-on supports"
+    min_point_version: Optional[int] = Field(
+        default=None, description="the minimum 2.1.x version this add-on supports",
+        gt=0
     )
-    max_point_version: Optional[float] = Field(
+    max_point_version: Optional[int] = Field(
         default=None,
         description=(
             "If negative, abs(n) is the maximum 2.1.x version this add-on supports. If"
             " positive, indicates version tested on, and is ignored"
         ),
     )
-    branch_index: Optional[float] = Field(
+    branch_index: Optional[int] = Field(
         default=None,
         description="AnkiWeb sends this to indicate which branch the user downloaded",
     )
